e1ba3436438f3fae3baad8beba200562238fd2e7
Richard Canoa <rcanoa@ucb.edu.bo>
2025-10-24 16:08:52 -0400

    Contexto Dapper

---
+++ SocialMedia.Core/Interfaces/IDapperContext.cs
@@ -0,0 +1,31 @@
+﻿using SocialMedia.Core.Enum;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Core.Interfaces
+{
+    public interface IDapperContext
+    {
+        DatabaseProvider Provider { get; }
+
+        Task<IEnumerable<T>> QueryAsync<T>(string sql, object? param = null,
+            CommandType commandType = CommandType.Text);
+
+        Task<T?> QueryFirstOrDefaultAsync<T>(string sql, object? param = null,
+            CommandType commandType = CommandType.Text);
+
+        Task<int> ExecuteAsync(string sql, object? param = null,
+            CommandType commandType = CommandType.Text);
+
+        Task<T> ExecuteScalarAsync<T>(string sql, object? param = null,
+            CommandType commandType = CommandType.Text);
+
+        void SetAmbientConnection(IDbConnection conn, IDbTransaction? tx);
+
+        void ClearAmbientConnection();
+    }
+}
