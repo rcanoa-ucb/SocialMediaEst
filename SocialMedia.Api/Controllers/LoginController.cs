0f6d6736c800b4c1bd85965972e038c828dde4c5
Richard Canoa <rcanoa@ucb.edu.bo>
2025-11-25 09:06:24 -0400

    Cambios en el post

--- SocialMedia.Api/Controllers/LoginController.cs
+++ SocialMedia.Api/Controllers/LoginController.cs
@@ -1,32 +1,30 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
-using SocialMedia.Core.CustomEntities;
 using SocialMedia.Core.Entities;
 using SocialMedia.Core.Interfaces;
 using System.IdentityModel.Tokens.Jwt;
 using System.Net;
 using System.Security.Claims;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SocialMedia.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class TokenController : ControllerBase
+    public class LoginController : ControllerBase
     {
         private readonly IConfiguration _configuration;
         private readonly ISecurityServices _securityServices;
-        public TokenController(IConfiguration configuration,
+        public LoginController(IConfiguration configuration,
             ISecurityServices securityServices)
         {
             _configuration = configuration;
             _securityServices = securityServices;
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Authentication([FromBody]UserLogin userLogin)
+        [HttpPost("User")]
+        public async Task<IActionResult> Authentication(UserLogin userLogin)
         {
             try
             {
@@ -46,31 +44,19 @@ namespace SocialMedia.Api.Controllers
             }
         }
 
-        [HttpPost("Login")]
-        public async Task<IActionResult> AuthenticationLogin(string user, string password)
+        [HttpGet]
+        public async Task<IActionResult> GetSec()
         {
             try
             {
-                //Si es usuario valido
-                var validation = await IsValidUser(new UserLogin() { User = user, Password = password });
-                if (validation.Item1)
-                {
-                    var token = GenerateToken(validation.Item2);
-                    return Ok(new { token });
-                }
-
-                return NotFound("Credenciales no válidas");
+                var res = await _securityServices.GetLoginByCredentials(new UserLogin() { User = "admin", Password = "admin" });
+               
+                return Ok(res);
             }
             catch (Exception err)
             {
                 return StatusCode((int)HttpStatusCode.InternalServerError, err.Message);
             }
-        }
-
-        private async Task<(bool, Security)> IsValidUser(UserLogin userLogin)
-        {
-            var user = await _securityServices.GetLoginByCredentials(userLogin);
-            return (user != null, user);
         }
 
         private string GenerateToken(Security security)
@@ -107,29 +93,10 @@ namespace SocialMedia.Api.Controllers
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
 
-        [HttpGet("Config")]
-        public async Task<IActionResult> GetConfig()
+        private async Task<(bool, Security)> IsValidUser(UserLogin userLogin)
         {
-            try
-            {
-                var connectionStringMySql = _configuration["ConnectionStrings:ConnectionMySql"];
-                var connectionStringSqlServer = _configuration["ConnectionStrings:ConnectionSqlServer"];
-
-                var result = new
-                {
-                    connectionStringMySql = connectionStringMySql ?? "My SQL NO CONFIGURADO",
-                    connectionStringSqlServer = connectionStringSqlServer ?? "SQL SERVER NO CONFIGURADO",
-                    AllConnectionStrings = _configuration.GetSection("ConnectionStrings").GetChildren().Select(x => new { Key = x.Key, Value = x.Value }),
-                    Environment = _configuration["ASPNETCORE_ENVIRONMENT"] ?? "ASPNETCORE_ENVIRONMENT NO CONFIGURADO",
-                    Authentication = _configuration.GetSection("Authentication").GetChildren().Select(x => new { Key = x.Key, Value = x.Value })
-                };
-
-                return Ok(result);
-            }
-            catch (Exception err)
-            {
-                return StatusCode((int)HttpStatusCode.InternalServerError, err.Message);
-            }
+            var user = await _securityServices.GetLoginByCredentials(userLogin);
+            return (user != null, user);
         }
     }
 }
