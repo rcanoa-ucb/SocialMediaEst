4805ec9ca27f501057c02b9aba2bc8b35991c065
Richard Canoa <rcanoa@ucb.edu.bo>
2026-03-09 14:18:53 -0400

    Initial commit

---
+++ SocialMedia.Api/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SocialMedia.Api.Controllers
+{
+    [Route("api/test")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                mensaje = "Conexión exitosa",
+                fecha = DateTime.Now
+            });
+        }
+
+        [HttpGet("con")]
+        public IActionResult Conexion()
+        {
+            return Ok(new { mensaje = "Conexión exitosa" });
+        }
+    }
+}
---
+++ SocialMedia.Infrastructure/Data/Security.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SocialMedia.Infrastructure.Data;
+
+public partial class Security
+{
+    public int Id { get; set; }
+
+    public string Login { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string Role { get; set; } = null!;
+}
